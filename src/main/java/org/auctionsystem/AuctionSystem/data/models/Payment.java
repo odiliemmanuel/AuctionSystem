97a77924be60746146c8930e884cfae74fe28e67
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-19 23:00:56 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/data/models/Payment.java
+++ src/main/java/org/auctionsystem/AuctionSystem/data/models/Payment.java
@@ -1,4 +1,13 @@
 package org.auctionsystem.AuctionSystem.data.models;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
 public class Payment {
+
+    @Id
+    private String id;
+    private double amount;
+    private PaymentStatus paymentStatus;
 }
