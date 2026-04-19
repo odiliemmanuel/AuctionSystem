41461fea5ecb5d37f0a2439ca2f10c6e9333aa37
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-19 22:58:15 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/data/repositories/PaymentRepository.java
+++ src/main/java/org/auctionsystem/AuctionSystem/data/repositories/PaymentRepository.java
@@ -1,9 +1,9 @@
 package org.auctionsystem.AuctionSystem.data.repositories;
 
-import org.auctionsystem.AuctionSystem.data.models.User;
+import org.auctionsystem.AuctionSystem.data.models.Payment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface PaymentRepository extends MongoRepository<Payment, String> {
 }
