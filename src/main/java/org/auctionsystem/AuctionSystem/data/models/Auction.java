35ddbf0d62d6d322f6e8c8cd6943b30048a9a117
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-19 22:48:23 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/data/models/Auction.java
+++ src/main/java/org/auctionsystem/AuctionSystem/data/models/Auction.java
@@ -1,9 +1,21 @@
 package org.auctionsystem.AuctionSystem.data.models;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
+import java.time.LocalDateTime;
 
+
+@Data
 public class Auction {
 
     @Id
     private String id;
+    private Product product;
+    private String sellerId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private AuctionStatus status;
+    private double currentHighestBid;
+    private User winner;
+
 }
--- src/main/java/org/auctionsystem/AuctionSystem/data/models/Bid.java
+++ src/main/java/org/auctionsystem/AuctionSystem/data/models/Bid.java
@@ -1,4 +1,16 @@
 package org.auctionsystem.AuctionSystem.data.models;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Data
 public class Bid {
+
+    @Id
+    private String id;
+    private double amount;
+    private LocalDateTime timeCreated;
+    private BidStatus bidStatus;
 }
