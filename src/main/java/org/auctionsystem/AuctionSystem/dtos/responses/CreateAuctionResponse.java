8fe95104def1bb88c8b74e41f12e9b3a1b888418
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-25 16:35:31 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/dtos/responses/CreateAuctionResponse.java
+++ src/main/java/org/auctionsystem/AuctionSystem/dtos/responses/CreateAuctionResponse.java
@@ -1,19 +1,26 @@
 package org.auctionsystem.AuctionSystem.dtos.responses;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.auctionsystem.AuctionSystem.data.models.AuctionStatus;
+import org.auctionsystem.AuctionSystem.data.models.Bid;
 import org.auctionsystem.AuctionSystem.data.models.Product;
 import org.auctionsystem.AuctionSystem.data.models.User;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
 
 @Data
 public class CreateAuctionResponse {
 
     private String id;
     private Product product;
+    private int numberOfBidders;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     private LocalDateTime startTime;
-    private LocalDateTime endTime;
-    private AuctionStatus status;
+
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    private LocalTime endTIme;
 
 }
