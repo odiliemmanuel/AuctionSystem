c6500641bdfd97a4e07a41e1a26bb3b26a4d4fba
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-25 15:39:16 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/dtos/requests/CreateAuctionRequest.java
+++ src/main/java/org/auctionsystem/AuctionSystem/dtos/requests/CreateAuctionRequest.java
@@ -1,7 +1,19 @@
 package org.auctionsystem.AuctionSystem.dtos.requests;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import java.time.LocalTime;
 
 @Data
 public class CreateAuctionRequest {
+
+    private String product;
+    private String sellerId;
+
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime startTime;
+    @JsonFormat(pattern = "HH:mm")
+    private LocalTime endTime;
+
+
 }
