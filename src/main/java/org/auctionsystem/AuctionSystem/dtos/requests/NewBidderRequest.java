c4d91b4390c0f596467991dddde0d2c476ea7046
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-25 16:35:34 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/dtos/requests/NewBidderRequest.java
+++ src/main/java/org/auctionsystem/AuctionSystem/dtos/requests/NewBidderRequest.java
@@ -1,4 +1,11 @@
 package org.auctionsystem.AuctionSystem.dtos.requests;
 
+import lombok.Data;
+
+@Data
 public class NewBidderRequest {
+
+    private String userId;
+    private String auctionId;
+    private double amount;
 }
