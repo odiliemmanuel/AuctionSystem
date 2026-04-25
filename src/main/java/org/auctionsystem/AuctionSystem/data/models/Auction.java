2629b3bd38124584aa24b34ad7181094c3871a54
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-25 15:32:37 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/data/models/Auction.java
+++ src/main/java/org/auctionsystem/AuctionSystem/data/models/Auction.java
@@ -2,9 +2,11 @@ package org.auctionsystem.AuctionSystem.data.models;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.time.LocalDateTime;
 
-
+@Document
 @Data
 public class Auction {
 
--- src/main/java/org/auctionsystem/AuctionSystem/data/models/Buyer.java
+++ src/main/java/org/auctionsystem/AuctionSystem/data/models/Buyer.java
@@ -1,7 +1,11 @@
 package org.auctionsystem.AuctionSystem.data.models;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+
+@Document
 @Data
 public class Buyer extends User{
 
