44a0a0456720d02f03d0aad366e2697bf8b72761
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-25 16:35:37 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/dtos/responses/NewBidderResponse.java
+++ src/main/java/org/auctionsystem/AuctionSystem/dtos/responses/NewBidderResponse.java
@@ -1,4 +1,22 @@
 package org.auctionsystem.AuctionSystem.dtos.responses;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.auctionsystem.AuctionSystem.data.models.BidStatus;
+
+import java.time.LocalDateTime;
+
+
+@Data
 public class NewBidderResponse {
+
+    private String bidId;
+    private String userId;
+    private String auctionId;
+    private double amount;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime timeCreated;
+
+    private BidStatus bidStatus;
 }
