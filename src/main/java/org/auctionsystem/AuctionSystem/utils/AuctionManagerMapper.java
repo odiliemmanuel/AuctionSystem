bea3a174d1b16c7cd87ea83476235110ec05187f
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-27 11:55:23 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/utils/AuctionManagerMapper.java
+++ src/main/java/org/auctionsystem/AuctionSystem/utils/AuctionManagerMapper.java
@@ -1,4 +1,23 @@
 package org.auctionsystem.AuctionSystem.utils;
 
+
+import org.auctionsystem.AuctionSystem.data.models.Auction;
+import org.auctionsystem.AuctionSystem.dtos.requests.CreateAuctionRequest;
+
 public class AuctionManagerMapper {
+
+
+    public static Auction mapCreateNewAuctionRequestToAuction(CreateAuctionRequest createAuctionRequest){
+
+    }
 }
+
+
+//private Product product;
+//private String sellerId;
+//
+//@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+//private LocalDateTime startTime;
+//
+//@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+//private LocalDateTime endTIme;
