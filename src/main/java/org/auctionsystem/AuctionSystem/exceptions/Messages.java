9f67de474710534e80366f3b6e3a6fbeb0380fc8
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-28 21:24:57 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/exceptions/Messages.java
+++ src/main/java/org/auctionsystem/AuctionSystem/exceptions/Messages.java
@@ -5,4 +5,5 @@ public class Messages {
     public static final String USER_ALREADY_EXISTS_EXCEPTION = "User already exist";
     public static final String USER_DOES_NOT_EXIST_EXCEPTION = "Cannot find user, please Sign up";
     public static final String INVALID_INPUT_EXCEPTION = "Invalid Input";
+    public static final String LOW_PRICE_OF_PRODUCT_EXCEPTION = "Products to be auctioned should be of reasonable price";
 }
