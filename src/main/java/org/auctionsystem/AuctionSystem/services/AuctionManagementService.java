714372461f79ab30318b5ec230f3e0a789c011b6
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-28 22:07:28 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/services/AuctionManagementService.java
+++ src/main/java/org/auctionsystem/AuctionSystem/services/AuctionManagementService.java
@@ -7,6 +7,7 @@ import org.auctionsystem.AuctionSystem.data.repositories.UserRepository;
 import org.auctionsystem.AuctionSystem.dtos.requests.CreateAuctionRequest;
 import org.auctionsystem.AuctionSystem.dtos.responses.CreateAuctionResponse;
 import org.auctionsystem.AuctionSystem.exceptions.Messages;
+import org.auctionsystem.AuctionSystem.exceptions.ProductAlreadyAuctionedBeforeBySellerException;
 import org.auctionsystem.AuctionSystem.utils.AuctionManagerMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
