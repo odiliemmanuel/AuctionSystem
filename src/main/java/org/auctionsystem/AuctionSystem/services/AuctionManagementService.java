86ea6db8c9fc391f2c5cbe7e31449f409a710e10
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-28 21:35:15 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/services/AuctionManagementService.java
+++ src/main/java/org/auctionsystem/AuctionSystem/services/AuctionManagementService.java
@@ -1,4 +1,13 @@
 package org.auctionsystem.AuctionSystem.services;
 
+import org.auctionsystem.AuctionSystem.data.repositories.AuctionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class AuctionManagementService{
+
+    @Autowired
+    AuctionRepository auctionRepository;
+
+
+    public static
 }
