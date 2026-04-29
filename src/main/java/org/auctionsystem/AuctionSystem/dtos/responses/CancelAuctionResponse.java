60f3d9c14dca0b8f1a8ecdae9566402cd78c6959
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-29 23:57:17 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/dtos/responses/CancelAuctionResponse.java
+++ src/main/java/org/auctionsystem/AuctionSystem/dtos/responses/CancelAuctionResponse.java
@@ -1,7 +1,10 @@
 package org.auctionsystem.AuctionSystem.dtos.responses;
 
 import lombok.Data;
+import org.auctionsystem.AuctionSystem.data.models.AuctionStatus;
 
 @Data
 public class CancelAuctionResponse {
+
+    private AuctionStatus status;
 }
