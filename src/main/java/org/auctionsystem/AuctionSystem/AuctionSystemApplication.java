36ad134a97cb7571c5ec077ee87c758d2d5c2810
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-29 07:52:13 +0100

    auction

---
+++ src/main/java/org/auctionsystem/AuctionSystem/AuctionSystemApplication.java
@@ -0,0 +1,17 @@
+package org.auctionsystem.AuctionSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionSystemApplication.class, args);
+	}
+
+}
+//https://github.com/odiliemmanuel/AuctionSystem.git
+
+//DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+//System.out.println(endTime.format(formatter));
---
+++ src/main/java/org/auctionsystem/AuctionSystem/utils/AuthenticationMapper.java
@@ -0,0 +1,52 @@
+package org.auctionsystem.AuctionSystem.utils;
+
+import org.auctionsystem.AuctionSystem.data.models.User;
+import org.auctionsystem.AuctionSystem.dtos.requests.UserSignUpRequest;
+import org.auctionsystem.AuctionSystem.dtos.responses.UserLoginResponse;
+import org.auctionsystem.AuctionSystem.dtos.responses.UserSignUpResponse;
+import org.mindrot.jbcrypt.BCrypt;
+
+public class AuthenticationMapper {
+
+
+    public static User mapUserSignUpRequestToUser(UserSignUpRequest userSignUpRequest){
+        User user = new User();
+
+        user.setFirstName(userSignUpRequest.getFirstName());
+        user.setLastName(userSignUpRequest.getLastName());
+        user.setPhoneNumber(userSignUpRequest.getPhoneNumber());
+        user.setUserName(userSignUpRequest.getUserName());
+        user.setEmailAddress(userSignUpRequest.getEmailAddress());
+        String hashedPassword = BCrypt.hashpw(userSignUpRequest.getPassword(), BCrypt.gensalt());
+        user.setPassword(hashedPassword);
+
+        return user;
+    }
+
+    public static UserSignUpResponse mapUserToSignUpResponse(User user){
+        UserSignUpResponse userSignUpResponse = new UserSignUpResponse();
+
+        userSignUpResponse.setUsername(user.getUserName());
+        userSignUpResponse.setPhoneNumber(user.getPhoneNumber());
+        userSignUpResponse.setMessage("Welcome to FlashCorp Blog App " + user.getUserName());
+
+        return userSignUpResponse;
+    }
+
+
+    public static UserLoginResponse mapUserLoginResponseToUser(User user){
+        UserLoginResponse userLoginResponse = new UserLoginResponse();
+
+        userLoginResponse.setUsername(user.getUserName());
+        userLoginResponse.setFirstName(user.getFirstName());
+        userLoginResponse.setPhoneNumber(user.getPhoneNumber());
+        userLoginResponse.setMessage("Welcome back " + user.getUserName());
+
+        return  userLoginResponse;
+    }
+
+
+
+
+}
+
