81427e2e96f33602b0da4a6d8a2db4f0a9d4f2da
odiliemmanuel <odiliejeh09@gmail.com>
2026-04-30 22:14:25 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/data/repositories/AuctionRepository.java
+++ src/main/java/org/auctionsystem/AuctionSystem/data/repositories/AuctionRepository.java
@@ -4,6 +4,9 @@ import org.auctionsystem.AuctionSystem.data.models.Auction;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AuctionRepository extends MongoRepository<Auction, String> {
+    void delete(Optional<Auction> auction);
 }
