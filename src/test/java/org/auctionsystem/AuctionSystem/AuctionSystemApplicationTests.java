f0f868fd78c02b45e7939f742899090a35138663
odiliemmanuel <odiliejeh09@gmail.com>
2026-05-01 19:07:34 +0100

    "auction"

---
+++ src/test/java/org/auctionsystem/AuctionSystem/AuctionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.auctionsystem.AuctionSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
