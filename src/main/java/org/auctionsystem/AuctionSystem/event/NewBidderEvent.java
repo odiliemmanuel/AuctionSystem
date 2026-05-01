5d30ceaabd72e0316e7a8798e140b6931dbab0ef
odiliemmanuel <odiliejeh09@gmail.com>
2026-05-01 09:54:06 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/event/NewBidderEvent.java
+++ src/main/java/org/auctionsystem/AuctionSystem/event/NewBidderEvent.java
@@ -3,9 +3,11 @@ package org.auctionsystem.AuctionSystem.event;
 public class NewBidderEvent extends Event{
 
     private String bidId;
+    private double amount;
 
-    public NewBidderEvent(String bidId){
+    public NewBidderEvent(String bidId, double amount){
         super("EVT-" + bidId, "NEW_BID");
         this.bidId = bidId;
+        this.amount = amount;
     }
 }
