1b06d4c9a747ea61187ac9bb02387e1558df9a82
odiliemmanuel <odiliejeh09@gmail.com>
2026-05-02 18:26:22 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/services/BidderManagementService.java
+++ src/main/java/org/auctionsystem/AuctionSystem/services/BidderManagementService.java
@@ -1,7 +1,14 @@
 package org.auctionsystem.AuctionSystem.services;
 
+import org.auctionsystem.AuctionSystem.data.repositories.BidderRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BidderManagementService {
+
+    private BidderRepository bidderRepository;
+
+    public BidderManagementService(BidderRepository bidderRepository) {
+        this.bidderRepository = bidderRepository;
+    }
 }
