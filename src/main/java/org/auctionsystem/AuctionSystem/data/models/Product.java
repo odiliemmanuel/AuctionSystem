e58b657c181985fee4425260e33fd16f52ee3e47
odiliemmanuel <odiliejeh09@gmail.com>
2026-05-02 23:43:22 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/data/models/Product.java
+++ src/main/java/org/auctionsystem/AuctionSystem/data/models/Product.java
@@ -12,7 +12,7 @@ public class Product {
     private String id;
     private String name;
     private String description;
-    private String price;
+    private double price;
     private String image;
 }
 
