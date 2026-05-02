b3396fefb8601c9f13205ee8a8ee2a2f8f2f1824
odiliemmanuel <odiliejeh09@gmail.com>
2026-05-02 18:09:31 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/data/models/Wallet.java
+++ src/main/java/org/auctionsystem/AuctionSystem/data/models/Wallet.java
@@ -1,8 +1,11 @@
 package org.auctionsystem.AuctionSystem.data.models;
 
 import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
 @Data
 public class Wallet {
 
+    private double balance;
 }
