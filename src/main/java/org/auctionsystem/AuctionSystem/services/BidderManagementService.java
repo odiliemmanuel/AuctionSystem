08573aa5505904ada1c6502ab5db1561f29565d1
odiliemmanuel <odiliejeh09@gmail.com>
2026-05-02 23:30:59 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/services/BidderManagementService.java
+++ src/main/java/org/auctionsystem/AuctionSystem/services/BidderManagementService.java
@@ -1,6 +1,8 @@
 package org.auctionsystem.AuctionSystem.services;
 
+import org.auctionsystem.AuctionSystem.data.repositories.AuctionRepository;
 import org.auctionsystem.AuctionSystem.data.repositories.BidderRepository;
+import org.auctionsystem.AuctionSystem.data.repositories.UserRepository;
 import org.auctionsystem.AuctionSystem.dtos.requests.NewBidderRequest;
 import org.auctionsystem.AuctionSystem.dtos.responses.NewBidderResponse;
 import org.springframework.stereotype.Service;
@@ -9,13 +11,18 @@ import org.springframework.stereotype.Service;
 public class BidderManagementService {
 
     private BidderRepository bidderRepository;
+    private AuctionRepository auctionRepository;
+    private UserRepository userRepository;
 
-    public BidderManagementService(BidderRepository bidderRepository) {
+    public BidderManagementService(BidderRepository bidderRepository,  AuctionRepository auctionRepository, UserRepository userRepository) {
         this.bidderRepository = bidderRepository;
+        this.auctionRepository = auctionRepository;
+        this.userRepository = userRepository;
     }
 
 
     public NewBidderResponse bidAuction(NewBidderRequest bidderRequest) {
+
 
     }
 }
