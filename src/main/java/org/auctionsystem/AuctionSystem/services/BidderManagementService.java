d29e11a3af916b73efefaf30a3412ff064377510
odiliemmanuel <odiliejeh09@gmail.com>
2026-05-02 23:35:27 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/services/BidderManagementService.java
+++ src/main/java/org/auctionsystem/AuctionSystem/services/BidderManagementService.java
@@ -1,12 +1,15 @@
 package org.auctionsystem.AuctionSystem.services;
 
 import org.auctionsystem.AuctionSystem.data.models.Auction;
+import org.auctionsystem.AuctionSystem.data.models.AuctionStatus;
 import org.auctionsystem.AuctionSystem.data.models.User;
 import org.auctionsystem.AuctionSystem.data.repositories.AuctionRepository;
 import org.auctionsystem.AuctionSystem.data.repositories.BidderRepository;
 import org.auctionsystem.AuctionSystem.data.repositories.UserRepository;
 import org.auctionsystem.AuctionSystem.dtos.requests.NewBidderRequest;
 import org.auctionsystem.AuctionSystem.dtos.responses.NewBidderResponse;
+import org.auctionsystem.AuctionSystem.exceptions.AuctionDoesNotExistException;
+import org.auctionsystem.AuctionSystem.exceptions.Messages;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -27,7 +30,9 @@ public class BidderManagementService {
         User user = userRepository.findById(bidderRequest.getUserId()).get();
         Auction auction = auctionRepository.findById(bidderRequest.getAuctionId()).get();
 
-
+        if(auction == null || auction.getStatus() == AuctionStatus.CLOSED) {
+            throw new AuctionDoesNotExistException(Messages.AUCTION_DOES_NOT_EXIST_EXCEPTION);
+        }
     }
 }
 //private String userId;
