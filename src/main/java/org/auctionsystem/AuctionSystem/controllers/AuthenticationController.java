f8a0e6c467b51cfbc455d1507132ebad4645c3ff
odiliemmanuel <odiliejeh09@gmail.com>
2026-05-03 12:51:23 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/controllers/AuthenticationController.java
+++ src/main/java/org/auctionsystem/AuctionSystem/controllers/AuthenticationController.java
@@ -8,11 +8,13 @@ import org.auctionsystem.AuctionSystem.services.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/users")
 public class AuthenticationController {
