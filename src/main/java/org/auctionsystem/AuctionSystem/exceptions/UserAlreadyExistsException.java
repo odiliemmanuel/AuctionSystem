db2e5b0bb493e074e32b8fcfd73e7a79cc00b5a9
odiliemmanuel <odiliejeh09@gmail.com>
2026-05-04 09:15:08 +0100

    "auction"

--- src/main/java/org/auctionsystem/AuctionSystem/exceptions/UserAlreadyExistsException.java
+++ src/main/java/org/auctionsystem/AuctionSystem/exceptions/UserAlreadyExistsException.java
@@ -1,3 +1,7 @@
 package org.auctionsystem.AuctionSystem.exceptions;
 
-publ
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
